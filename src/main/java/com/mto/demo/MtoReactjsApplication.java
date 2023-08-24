09b32d8553c8d614fe384d2e048bf7e9f7171cf5
ravi <ravi27mca@gmail.com>
2023-08-24 19:44:24 +0530

    react js with spring boot

---
+++ src/main/java/com/mto/demo/MtoReactjsApplication.java
@@ -0,0 +1,13 @@
+package com.mto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+
+@SpringBootApplication
+public class MtoReactjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtoReactjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mto/demo/MtoReactjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtoReactjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
